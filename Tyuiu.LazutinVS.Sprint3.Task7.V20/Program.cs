8d75b499790ab5e93fcc108e7e002a8a9f85a880
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-10-31 21:13:38 +0500

    Task7

---
+++ Tyuiu.LazutinVS.Sprint3.Task7.V20/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.LazutinVS.Sprint3.Task7.V20.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+        int len = ds.GetMassFunction(startValue, stopValue).Length;
+        double[] valueArray;
+        valueArray = new double[len];
+        valueArray = ds.GetMassFunction(startValue, stopValue);
+        Console.WriteLine("********************************************************************************");
+        Console.WriteLine("* Результат:                                                                   *");
+        Console.WriteLine("********************************************************************************");
+        Console.WriteLine(" +-----------+-----------+");
+        Console.WriteLine(" |     X     |    F(x)   |");
+        Console.WriteLine(" +-----------+-----------+");
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine(" |{0,5:d}       |  {1, 5:f2}   |", startValue, valueArray[i]);
+            startValue++;
+        }
+        Console.WriteLine(" +-----------+-----------+");
+        Console.ReadKey();
+    }
+}
