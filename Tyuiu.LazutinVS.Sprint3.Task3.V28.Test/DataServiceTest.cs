c84afb45e40301fc1551a5893f6af2afa5b38cc1
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-10-31 18:16:34 +0500

    Task3

---
+++ Tyuiu.LazutinVS.Sprint3.Task3.V28.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.LazutinVS.Sprint3.Task3.V28.Lib;
+namespace Tyuiu.LazutinVS.Sprint3.Task3.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+
+        [TestMethod]
+        public void Test1()
+        {
+            DataService ds = new DataService();
+        }
+    }
+}
---
+++ Tyuiu.LazutinVS.Sprint3.Task3.V28/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.LazutinVS.Sprint3.Task3.V28.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("Введите строку: ");
+        string a = Console.ReadLine();
+        Console.WriteLine("Введите символ: ");
+        char start = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.ReplaceNumOnChar(a, start));
+        Console.ReadKey();
+    }
+}
